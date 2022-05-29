5e3045b14f4bec689f11480f885c8eca98a4d717
windaaasaputri <windaekasaputri30@gmail.com>
2022-05-29 07:18:08 -0600

    memperbaiki error

--- database/database.php
+++ database/database.php
@@ -17,7 +17,7 @@ class Database{
     public function getConnection(){
         $this->connection = null;
         try{
-            $this->connection = new PDO("mysql:host=" . $this->host . ";nama_database=" . $this->nama_database, $this->username, $this->password);
+            $this->connection = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->nama_database, $this->username, $this->password);
             $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e){
             echo "Error pada koneksi ;" . $e->getMessage();
