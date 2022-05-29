87dab412f67b8904a038f0207a630a6763fa3f38
windaaasaputri <windaekasaputri30@gmail.com>
2022-05-29 07:43:22 -0600

    pemecahan komponen dan pemberian class active untuk nav.php

---
+++ pages/routes.php
@@ -0,0 +1,21 @@
+<?php 
+if(isset($_GET['page'])){
+    $page = $_GET['page'];
+    switch[$page]{
+        case '':
+            include "dashboard.php";
+            break;
+        case 'karyawan':
+            include "karyawan.php";
+            break;
+        case 'bagian':
+            include "bagian.php";
+            break;
+        default:
+            include "dashboard.php";
+            break;
+    }
+} else {
+    include "dashboard.php";
+}
+?> 
