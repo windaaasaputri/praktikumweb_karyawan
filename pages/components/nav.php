c8497748817e018d22b6b18c5e5e21e1d3b287ed
windaaasaputri <windaekasaputri30@gmail.com>
2022-05-29 08:02:13 -0600

    menambahkan OR untuk karyawancreate

--- pages/components/nav.php
+++ pages/components/nav.php
@@ -8,7 +8,7 @@
             </a>
           </li>
           <li class="nav-item">
-          <a class="nav-link <?php echo $_GET['page'] == 'karyawan' ? ' active' : '' ?>" href="?page=karyawan">
+          <a class="nav-link <?php echo $_GET['page'] == 'karyawan' || $_GET['page'] == 'karyawancreate' ? ' active' : '' ?>" href="?page=karyawan">
               <span data-feather="users"></span>
               Data Karyawan
             </a>
